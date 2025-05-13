6a5e4e95319217067be71bf156a9072f4d6f31f9
Your Name <Dianzahidd@gmail.com>
2025-05-13 17:27:33 -0400

    implement selectionSort

--- src/components/AlgorithmVisualizer.tsx
+++ src/components/AlgorithmVisualizer.tsx
@@ -6,13 +6,14 @@ interface AlgorithmVisualizerProps {
 }
 
 //make a component named AlgorithmVisualizer with a prop type selectedAlgorithm
-const AlgorithmVisualizer: React.FC<AlgorithmVisualizerProps> = ({ selectedAlgorithm }) => {
+const AlgorithmVisualizer: React.FC<AlgorithmVisualizerProps> = () => {
   //state variables ** Important for UI to re render**
   const [array, setArray] = useState<number[]>([]) //initialize array to only hold numbers and empty 
   const [isPlaying, setIsPlaying] = useState(false) //initialize to flase (not playing or paused)
   const [speed, setSpeed] = useState(1) //initial speed is 1x
   const [currentStep, setCurrentStep] = useState(0)
   const [sortingSteps, setSortingSteps] = useState<number[][]>([])
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState('bubble-sort')
 
   const generateNewArray = () => {
     const newArray = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100)) // Creats a new array size 10 with numbers from 0 to 99
@@ -40,7 +41,7 @@ const AlgorithmVisualizer: React.FC<AlgorithmVisualizerProps> = ({ selectedAlgor
       }
     } while (swapped)
     
-    console.log(steps.length);
+    //console.log(steps.length);
     return steps
   }
 
@@ -59,11 +60,33 @@ const AlgorithmVisualizer: React.FC<AlgorithmVisualizerProps> = ({ selectedAlgor
       arr[i+1] = key
       steps.push(arr.slice()) //show element shift 
     }
-    console.log(steps);
+    //console.log(steps);
     return steps;
   }
 
-  console.log(insertionSort([3,2,1,8,53,7,9,3]));
+  //selectionSort implementation
+  const selectionSort = (arr: number[]) => {
+
+    const steps: number[][] = [arr.slice()]
+
+    for (let i = 0; i<arr.length; i++){
+      let min = i
+      for(let j=i+1; j < arr.length; j++){
+        if(arr[j] < arr[min]){
+          min = j
+        }
+      }
+      if (i !== min){
+        [arr[i], arr[min]] = [arr[min], arr[i]];
+        steps.push(arr.slice());
+      }
+    }
+    console.log(steps)
+      return steps
+  }
+
+
+
 
 //runs everytime a component renders or a value changes 
   useEffect(() => {
@@ -71,8 +94,8 @@ const AlgorithmVisualizer: React.FC<AlgorithmVisualizerProps> = ({ selectedAlgor
       const timer = setTimeout(() => {
         setCurrentStep(prev => prev + 1) //move to the next step
         setArray(sortingSteps[currentStep + 1]) //set the array to the next one
-        console.log(currentStep);
-        console.log(array)
+       // console.log(currentStep);
+       // console.log(array)
       }, 1000 / speed) //1s per step
       return () => clearTimeout(timer) //clear so no stacking 
     } else if (currentStep >= sortingSteps.length - 1) { //last step stop playing
@@ -82,8 +105,16 @@ const AlgorithmVisualizer: React.FC<AlgorithmVisualizerProps> = ({ selectedAlgor
 
   //called when play is clicked
   const handlePlay = () => {
-    if (!isPlaying && currentStep === 0) { //makes sure INITIAL state 
-      const steps = bubbleSort([...array]) //calls bubble sort with a copy of 'array'
+    if (!isPlaying && currentStep === 0) { //makes sure INITIAL state
+      let steps: number[][] = []
+      if(selectedAlgorithm === 'bubble-sort'){
+       steps = bubbleSort([...array]) //calls bubble sort with a copy of 'array'
+       } else if (selectedAlgorithm === 'insertion-sort'){
+        steps = insertionSort([...array])
+      }
+      else if (selectedAlgorithm === 'selection-sort'){
+        steps = selectionSort([...array])
+      }
       setSortingSteps(steps) // allows useEffects to now run
     }
     setIsPlaying(!isPlaying) //change the state of the button 
@@ -94,6 +125,10 @@ const AlgorithmVisualizer: React.FC<AlgorithmVisualizerProps> = ({ selectedAlgor
       <div className="bg-flex flex-col space-y-4"> {/*The flex container for the Visualizer -- top are buttons -- middle is the graph -- bottom is the speed  */}
         <div className="flex justify-between items-center"> {/* top div and flex container for buttons */}
           <select
+            value={selectedAlgorithm}
+            onChange={(e) => {
+              setSelectedAlgorithm(e.target.value) //set the state to the user chosen
+            }}
             className="bg-black px-4 py-2 text-white rounded-md cursor-pointer border-black border border-r-8"
           > {/* selector for type of sorting algorithm */}
             <option value="bubble-sort">Bubble Sort</option>
